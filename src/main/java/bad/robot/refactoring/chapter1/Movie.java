c6e70880eae6c0867044b60e016ee0277fafcf8d
Toby Weston <toby.weston@gmail.com>
2011-10-23 14:39:11 +0100

    move method (pg.142) to move the getCharge method onto Price. Left the delegate method in Movie though.

--- src/main/java/bad/robot/refactoring/chapter1/Movie.java
+++ src/main/java/bad/robot/refactoring/chapter1/Movie.java
@@ -7,7 +7,7 @@ public class Movie {
     public static final int NEW_RELEASE = 1;
 
     private String title;
-    private Price price;
+    Price price;
 
     public Movie(String title, int priceCode) {
         this.title = title;
@@ -39,23 +39,7 @@ public class Movie {
     }
 
     public double getCharge(int daysRented) {
-        double amount = 0;
-        switch (getPriceCode()) {
-            case REGULAR:
-                amount += 2;
-                if (daysRented > 2)
-                    amount += (daysRented - 2) * 1.5;
-                break;
-            case NEW_RELEASE:
-                amount += daysRented * 3;
-                break;
-            case CHILDREN:
-                amount += 1.5;
-                if (daysRented > 3)
-                    amount += (daysRented - 3) * 1.5;
-                break;
-        }
-        return amount;
+        return price.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints(int daysRented) {
