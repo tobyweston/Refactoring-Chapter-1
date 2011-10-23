ef3025231be422912ce4ddd0280717c8c50a0c39
Toby Weston <toby.weston@gmail.com>
2011-10-23 14:46:50 +0100

    working towards replace conditional with polymorphism for the getFrequentRenterPoints method. First step, move method (pg.142).

--- src/main/java/bad/robot/refactoring/chapter1/Movie.java
+++ src/main/java/bad/robot/refactoring/chapter1/Movie.java
@@ -43,8 +43,7 @@ public class Movie {
     }
 
     public int getFrequentRenterPoints(int daysRented) {
-        if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
-            return 2;
-        return 1;
+        return price.getFrequentRenterPoints(daysRented);
     }
+
 }
--- src/main/java/bad/robot/refactoring/chapter1/Price.java
+++ src/main/java/bad/robot/refactoring/chapter1/Price.java
@@ -4,4 +4,10 @@ public abstract class Price {
     abstract int getPriceCode();
 
     abstract double getCharge(int daysRented);
+
+    int getFrequentRenterPoints(int daysRented) {
+        if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
+            return 2;
+        return 1;
+    }
 }
