570ffd71e44d85c7eb07f1e2b1b4bca46db493b4
Toby Weston <toby.weston@gmail.com>
2011-10-23 14:48:22 +0100

    replace conditional with polymorphism for the getFrequentRenterPoints method. Up to pg.49 in the book.

--- src/main/java/bad/robot/refactoring/chapter1/NewReleasePrice.java
+++ src/main/java/bad/robot/refactoring/chapter1/NewReleasePrice.java
@@ -11,4 +11,11 @@ public class NewReleasePrice extends Price {
     public double getCharge(int daysRented) {
         return daysRented * 3;
     }
+
+    @Override
+    int getFrequentRenterPoints(int daysRented) {
+        if (daysRented > 1)
+            return 2;
+        return 1;
+    }
 }
