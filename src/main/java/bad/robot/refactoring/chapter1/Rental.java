69859dd14a232e00264afef3caa396988f0873cf
Toby Weston <toby.weston@gmail.com>
2011-10-23 14:01:30 +0100

    replacing conditional with polymorphism; move method (pg.142) getCharge from Rental to Movie. Left a delegate method on Rental so changes to customer are minimised and increase encapsulation

--- src/main/java/bad/robot/refactoring/chapter1/Rental.java
+++ src/main/java/bad/robot/refactoring/chapter1/Rental.java
@@ -1,11 +1,8 @@
 package bad.robot.refactoring.chapter1;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Rental {
 
-    private Movie movie;
+    Movie movie;
     private int daysRented;
 
     public Rental(Movie movie, int daysRented) {
@@ -22,23 +19,7 @@ public class Rental {
     }
 
     public double getCharge() {
-        double amount = 0;
-        switch (getMovie().getPriceCode()) {
-            case Movie.REGULAR:
-                amount += 2;
-                if (getDaysRented() > 2)
-                    amount += (getDaysRented() - 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                amount += getDaysRented() * 3;
-                break;
-            case Movie.CHILDREN:
-                amount += 1.5;
-                if (getDaysRented() > 3)
-                    amount += (getDaysRented() - 3) * 1.5;
-                break;
-        }
-        return amount;
+        return movie.getCharge(daysRented);
     }
 
     public int getFrequentRenterPoints() {
