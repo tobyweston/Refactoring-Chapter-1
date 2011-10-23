74b34ba9ae870c6d5e4a5d0b8538dda043b77846
Toby Weston <toby.weston@gmail.com>
2011-10-23 13:34:13 +0100

    extract method (pg.110) to isolate the frequent renter points calculation (part 1)

--- src/main/java/bad/robot/refactoring/chapter1/Customer.java
+++ src/main/java/bad/robot/refactoring/chapter1/Customer.java
@@ -26,12 +26,7 @@ public class Customer {
 
         String result = "Rental record for " + getName() + "\n";
         for (Rental rental : rentals) {
-
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
-                frequentRenterPoints++;
+            frequentRenterPoints += getFrequentRenterPoints(rental);
 
             // show figures for this rental
             result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n";
@@ -43,6 +38,14 @@ public class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
 
         return result;
+    }
+
+    private int getFrequentRenterPoints(Rental rental) {
+        int frequentRenterPoints = 0;
+        frequentRenterPoints++;
+        if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
+            frequentRenterPoints++;
+        return frequentRenterPoints;
     }
 
 }
