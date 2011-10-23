445fd49c955d977f5225b8a0f6bfc48cbfd1c8c4
Toby Weston <toby.weston@gmail.com>
2011-10-23 14:44:13 +0100

    replace condition with polymorphism (pg.255), for the remaining Price subclasses. Made the super class getCharge method abstract

--- src/main/java/bad/robot/refactoring/chapter1/ChildrensPrice.java
+++ src/main/java/bad/robot/refactoring/chapter1/ChildrensPrice.java
@@ -6,4 +6,12 @@ public class ChildrensPrice extends Price {
     public int getPriceCode() {
         return Movie.CHILDREN;
     }
+
+    @Override
+    double getCharge(int daysRented) {
+        double amount = 1.5;
+        if (daysRented > 3)
+            amount += (daysRented - 3) * 1.5;
+        return amount;
+    }
 }
--- src/main/java/bad/robot/refactoring/chapter1/NewReleasePrice.java
+++ src/main/java/bad/robot/refactoring/chapter1/NewReleasePrice.java
@@ -6,4 +6,9 @@ public class NewReleasePrice extends Price {
     public int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    @Override
+    public double getCharge(int daysRented) {
+        return daysRented * 3;
+    }
 }
