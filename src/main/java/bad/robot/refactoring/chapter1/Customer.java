87052b984908bb9e83fe6b754596831eb40ad1b3
Toby Weston <toby.weston@gmail.com>
2011-10-23 13:49:43 +0100

    added htmlStatement method to show how the previous refactoring makes it easier to adapt the program (up to pg. 33)

--- src/main/java/bad/robot/refactoring/chapter1/Customer.java
+++ src/main/java/bad/robot/refactoring/chapter1/Customer.java
@@ -22,15 +22,21 @@ public class Customer {
 
     public String statement() {
         String result = "Rental record for " + getName() + "\n";
-        for (Rental rental : rentals) {
-            // show figures for this rental
+        for (Rental rental : rentals)
             result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "\n";
-        }
-
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
-
         return result;
+    }
+
+    public String htmlStatement() {
+        String result = "<h1>Rental record for <b>" + getName() + "</b></h1>\n";
+        for (Rental rental : rentals)
+            result += "<p>" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getCharge()) + "</p>\n";
+        result += "<p>Amount owed is <b>" + String.valueOf(getTotalCharge()) + "</b></p>\n";
+        result += "<p>You earned <b>" + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points</b></p>";
+        return result;
+
     }
 
     private double getTotalCharge() {
