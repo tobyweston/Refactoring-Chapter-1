e69a71e29b7e1d556afd932c9264479a37c1a80f
Toby Weston <toby.weston@gmail.com>
2011-10-23 14:03:39 +0100

    replacing conditional with polymorphism; move method (pg.142) getFrequentRenterPoints from Rental to Movie. Left a delegate method on Rental again. Up to pg.37 in the book.

--- src/main/java/bad/robot/refactoring/chapter1/Movie.java
+++ src/main/java/bad/robot/refactoring/chapter1/Movie.java
@@ -45,4 +45,10 @@ public class Movie {
         }
         return amount;
     }
+
+    public int getFrequentRenterPoints(int daysRented) {
+        if (priceCode == Movie.NEW_RELEASE && daysRented > 1)
+            return 2;
+        return 1;
+    }
 }
--- src/main/java/bad/robot/refactoring/chapter1/Rental.java
+++ src/main/java/bad/robot/refactoring/chapter1/Rental.java
@@ -23,8 +23,6 @@ public class Rental {
     }
 
     public int getFrequentRenterPoints() {
-        if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1)
-            return 2;
-        return 1;
+        return movie.getFrequentRenterPoints(daysRented);
     }
 }
