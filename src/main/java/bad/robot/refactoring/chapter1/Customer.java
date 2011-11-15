c159308be1635fccbb6246521caec1e27469bf26
Toby Weston <toby.weston@gmail.com>
2011-11-15 16:00:55 +0000

    change method implementation, simplified renter points calculation (no need for the variable any more). Just leaves nasty comments

--- src/main/java/bad/robot/refactoring/chapter1/Customer.java
+++ src/main/java/bad/robot/refactoring/chapter1/Customer.java
@@ -42,13 +42,11 @@ public class Customer {
     }
 
     private int getFrequentRenterPoints(Rental rental) {
-        int frequentRenterPoints = 0;
         // add frequent renter points
-        frequentRenterPoints++;
         // add bonus for a two day new release rental
         if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
-            frequentRenterPoints++;
-        return frequentRenterPoints;
+            return 2;
+        return 1;
     }
 
 }
