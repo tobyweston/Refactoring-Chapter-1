0cb4ac4017cacfbbee54c9ae718ff0f6d3989f27
Toby Weston <toby.weston@gmail.com>
2011-11-15 17:11:27 +0000

    push the varying behaviour into the subtype, still on pg. 49

--- src/main/java/bad/robot/refactoring/chapter1/NewReleasePrice.java
+++ src/main/java/bad/robot/refactoring/chapter1/NewReleasePrice.java
@@ -10,4 +10,11 @@ class NewReleasePrice extends Price {
     double getCharge(int daysRented) {
         return daysRented * 3;
     }
+
+    @Override
+    int getFrequentRenterPoints(int daysRented) {
+        if (daysRented > 1)
+            return 2;
+        return 1;
+    }
 }
--- src/main/java/bad/robot/refactoring/chapter1/Price.java
+++ src/main/java/bad/robot/refactoring/chapter1/Price.java
@@ -7,8 +7,6 @@ public abstract class Price {
     abstract double getCharge(int daysRented);
 
     int getFrequentRenterPoints(int daysRented) {
-        if (getPriceCode() == Movie.NEW_RELEASE && daysRented > 1)
-            return 2;
         return 1;
     }
 }
