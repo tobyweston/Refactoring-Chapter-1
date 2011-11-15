133c080fae8603758d5938b03077450e61f91b13
Toby Weston <toby.weston@gmail.com>
2011-11-15 17:05:58 +0000

    moving switch statements into subtypes, slowing replacing the conditional... pg. 47.

--- src/main/java/bad/robot/refactoring/chapter1/Price.java
+++ src/main/java/bad/robot/refactoring/chapter1/Price.java
@@ -1,16 +1,12 @@
 package bad.robot.refactoring.chapter1;
 
 public abstract class Price {
+
     abstract int getPriceCode();
 
     double getCharge(int daysRented) {
         double amount = 0;
         switch (getPriceCode()) {
-            case Movie.REGULAR:
-                amount += 2;
-                if (daysRented > 2)
-                    amount += (daysRented - 2) * 1.5;
-                break;
             case Movie.NEW_RELEASE:
                 amount += daysRented * 3;
                 break;
--- src/main/java/bad/robot/refactoring/chapter1/RegularPrice.java
+++ src/main/java/bad/robot/refactoring/chapter1/RegularPrice.java
@@ -1,8 +1,17 @@
 package bad.robot.refactoring.chapter1;
 
 class RegularPrice extends Price {
+
     @Override
     int getPriceCode() {
         return Movie.REGULAR;
+    }
+
+    @Override
+    double getCharge(int daysRented) {
+        double amount = 2;
+        if (daysRented > 2)
+            amount += (daysRented - 2) * 1.5;
+        return amount;
     }
 }
