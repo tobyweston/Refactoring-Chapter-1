1d49c7dc7ae9b697da36cf265750bac684ce810a
Toby Weston <toby.weston@gmail.com>
2011-11-15 15:37:10 +0000

    move method, rename method. Moved amountFor(rental) into Rental to enable rental.getCharge(). Up to pg 17.

--- src/main/java/bad/robot/refactoring/chapter1/Rental.java
+++ src/main/java/bad/robot/refactoring/chapter1/Rental.java
@@ -1,8 +1,5 @@
 package bad.robot.refactoring.chapter1;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Rental {
 
     private Movie movie;
@@ -21,4 +18,23 @@ public class Rental {
         return daysRented;
     }
 
+    public double getCharge() {
+        double amount = 0;
+        switch (getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                amount += 2;
+                if (getDaysRented() > 2)
+                    amount += (getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount += getDaysRented() * 3;
+                break;
+            case Movie.CHILDREN:
+                amount += 1.5;
+                if (getDaysRented() > 3)
+                    amount += (getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return amount;
+    }
 }
