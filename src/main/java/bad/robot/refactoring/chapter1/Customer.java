b4172bff7b86f2f31e24a5e0031cad177fcd2d61
Toby Weston <toby.weston@gmail.com>
2011-11-15 16:01:24 +0000

    removed comments, pg. 23

--- src/main/java/bad/robot/refactoring/chapter1/Customer.java
+++ src/main/java/bad/robot/refactoring/chapter1/Customer.java
@@ -42,8 +42,6 @@ public class Customer {
     }
 
     private int getFrequentRenterPoints(Rental rental) {
-        // add frequent renter points
-        // add bonus for a two day new release rental
         if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
             return 2;
         return 1;
