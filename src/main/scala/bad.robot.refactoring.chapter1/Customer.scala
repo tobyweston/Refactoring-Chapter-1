f950274127db1c64acb11f5d311f750a52b735c7
toby.weston <toby.weston@db.com>
2014-01-08 10:18:16 +0000

    extract method as described on pg.17

--- src/main/scala/bad.robot.refactoring.chapter1/Customer.scala
+++ src/main/scala/bad.robot.refactoring.chapter1/Customer.scala
@@ -13,19 +13,7 @@ case class Customer(name: String, rentals: Seq[Rental] = Seq()) {
     var result = "Rental record for " + name + "\n"
 
     for (rental <- rentals) {
-      var amount: Double = 0
-      rental.movie.priceCode match {
-        case Movie.REGULAR =>
-          amount += 2
-          if (rental.daysRented > 2)
-            amount += (rental.daysRented - 2) * 1.5
-        case Movie.NEW_RELEASE =>
-          amount += rental.daysRented * 3
-        case Movie.CHILDREN =>
-          amount += 1.5
-          if (rental.daysRented > 3)
-            amount += (rental.daysRented - 3) * 1.5
-      }
+      var amount: Double = amountFor(rental)
       frequentRenterPoints += 1
       if (rental.movie.priceCode == Movie.NEW_RELEASE && rental.daysRented > 1)
         frequentRenterPoints += 1; frequentRenterPoints - 1
@@ -35,6 +23,23 @@ case class Customer(name: String, rentals: Seq[Rental] = Seq()) {
     result += "Amount owed is " + String.valueOf(totalAmount) + "\n"
     result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points"
     return result
+  }
+
+  private def amountFor(rental: Rental): Double = {
+    var amount: Double = 0
+    rental.movie.priceCode match {
+      case Movie.REGULAR =>
+        amount += 2
+        if (rental.daysRented > 2)
+          amount += (rental.daysRented - 2) * 1.5
+      case Movie.NEW_RELEASE =>
+        amount += rental.daysRented * 3
+      case Movie.CHILDREN =>
+        amount += 1.5
+        if (rental.daysRented > 3)
+          amount += (rental.daysRented - 3) * 1.5
+    }
+    amount
   }
 
 }
