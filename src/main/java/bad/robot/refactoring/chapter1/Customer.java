1e7a83136b6e9d0ab735bd7d03b87a13ec141ed4
Zak <slocumz@ferris.edu>
2022-05-08 13:27:03 -0400

    Initializing and extract method

--- src/main/java/bad/robot/refactoring/chapter1/Customer.java
+++ src/main/java/bad/robot/refactoring/chapter1/Customer.java
@@ -26,22 +26,7 @@ public class Customer {
 
         String result = "Rental record for " + getName() + "\n";
         for (Rental rental : rentals) {
-            double amount = 0;
-            switch (rental.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    amount += 2;
-                    if (rental.getDaysRented() > 2)
-                        amount += (rental.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    amount += rental.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDREN:
-                    amount += 1.5;
-                    if (rental.getDaysRented() > 3)
-                        amount += (rental.getDaysRented() - 3) * 1.5;
-                    break;
-            }
+            double amount = getAmount(rental);
 
             // add frequent renter points
             frequentRenterPoints++;
@@ -60,4 +45,24 @@ public class Customer {
 
         return result;
     }
+
+	private double getAmount(Rental rental) {
+		double amount = 0;
+		switch (rental.getMovie().getPriceCode()) {
+		    case Movie.REGULAR:
+		        amount += 2;
+		        if (rental.getDaysRented() > 2)
+		            amount += (rental.getDaysRented() - 2) * 1.5;
+		        break;
+		    case Movie.NEW_RELEASE:
+		        amount += rental.getDaysRented() * 3;
+		        break;
+		    case Movie.CHILDREN:
+		        amount += 1.5;
+		        if (rental.getDaysRented() > 3)
+		            amount += (rental.getDaysRented() - 3) * 1.5;
+		        break;
+		}
+		return amount;
+	}
 }
