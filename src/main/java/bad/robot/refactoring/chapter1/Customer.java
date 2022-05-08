78878cf88cf11910e4316d9bd57e3d3f1a80cc79
Zak <slocumz@ferris.edu>
2022-05-08 13:56:01 -0400

    Replace Temp with Query

--- src/main/java/bad/robot/refactoring/chapter1/Customer.java
+++ src/main/java/bad/robot/refactoring/chapter1/Customer.java
@@ -26,8 +26,6 @@ public class Customer {
 
 		String result = "Rental record for " + getName() + "\n";
 		for (Rental rental : rentals) {
-			double amount = rental.getChargeFor();
-
 			// add frequent renter points
 			frequentRenterPoints++;
 			// add bonus for a two day new release rental
@@ -35,9 +33,9 @@ public class Customer {
 				frequentRenterPoints++;
 
 			// show figures for this rental
-			result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(amount) + "\n";
+			result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getChargeFor()) + "\n";
 
-			totalAmount += amount;
+			totalAmount += rental.getChargeFor();
 		}
 
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
