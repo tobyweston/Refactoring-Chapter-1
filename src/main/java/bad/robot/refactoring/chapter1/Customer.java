bc62da93b96821ce85bae625b64a2e008577899a
brandonedgarton <61728761+brandonedgarton@users.noreply.github.com>
2024-01-26 18:40:34 -0900

    v1.1

--- src/main/java/bad/robot/refactoring/chapter1/Customer.java
+++ src/main/java/bad/robot/refactoring/chapter1/Customer.java
@@ -26,33 +26,12 @@ public class Customer {
 
         String result = "Rental record for " + getName() + "\n";
         for (Rental rental : rentals) {
-            double amount = 0;
-            switch (rental.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    amount += 2;
-                    if (rental.getDaysRented() > 2)
-                        amount += (rental.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    amount += rental.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDREN:
-                    amount += 1.5;
-                    if (rental.getDaysRented() > 3)
-                        amount += (rental.getDaysRented() - 3) * 1.5;
-                    break;
-            }
-
-            // add frequent renter points
-            frequentRenterPoints++;
-            // add bonus for a two day new release rental
-            if (rental.getMovie().getPriceCode() == Movie.NEW_RELEASE && rental.getDaysRented() > 1)
-                frequentRenterPoints++;
+            frequentRenterPoints += rental.calculateFrequentRenterPoints();
 
             // show figures for this rental
-            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(amount) + "\n";
+            result += "\t" + rental.getMovie().getTitle() + "\t" + String.valueOf(rental.getChargeFor()) + "\n";
 
-            totalAmount += amount;
+            totalAmount += rental.getChargeFor();
         }
 
         result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
--- src/main/java/bad/robot/refactoring/chapter1/Rental.java
+++ src/main/java/bad/robot/refactoring/chapter1/Rental.java
@@ -21,4 +21,30 @@ public class Rental {
         return daysRented;
     }
 
+	double getChargeFor() {
+		double amount = 0;
+		switch (getMovie().getPriceCode()) {
+		    case Movie.REGULAR:
+		        amount += 2;
+		        if (getDaysRented() > 2)
+		            amount += (getDaysRented() - 2) * 1.5;
+		        break;
+		    case Movie.NEW_RELEASE:
+		        amount += getDaysRented() * 3;
+		        break;
+		    case Movie.CHILDREN:
+		        amount += 1.5;
+		        if (getDaysRented() > 3)
+		            amount += (getDaysRented() - 3) * 1.5;
+		        break;
+		}
+		return amount;
+	}
+
+	int calculateFrequentRenterPoints() {
+		if (getMovie().getPriceCode() == Movie.NEW_RELEASE && getDaysRented() > 1)
+		    return 2;
+		return 1;
+	}
+
 }
